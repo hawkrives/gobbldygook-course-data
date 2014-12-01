e02d909b9d620090a930810bc6919b16f4eaebf8
Hawken Rives <hawkrives@gmail.com>
2014-12-01 14:48:40 -0600

    Remove nom stuff

--- scripts/create-github-release.js
+++
@@ -1,8 +0,0 @@
-var vinyl = require('vinyl-fs');
-var release = require('github-release');
-var pkg = require('../package.json');
-
-var sourcePath = '../dist/data.tar.gz';
-
-vinyl(sourcePath)
-	.pipe(githubRelease(pkg));
