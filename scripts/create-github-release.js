9dbe398be1652a94cd6d1c9c337bc0a89332df64
Hawken Rives <hawkrives@gmail.com>
2014-12-01 11:56:30 -0600

    ... fix a typo

--- scripts/create-github-release.js
+++ scripts/create-github-release.js
@@ -1,6 +1,6 @@
 var vinyl = require('vinyl-fs');
 var release = require('github-release');
-var pkg = require('package.json');
+var pkg = require('../package.json');
 
 var sourcePath = '../dist/data.tar.gz';
 
