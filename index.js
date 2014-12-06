d1605c9a6af4c1e3f5d4c08559bc77af5ce41c6f
Hawken Rives <hawkrives@gmail.com>
2014-12-05 19:26:01 -0600

    Update info.json

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+	departmentNameToAbbr: require('mappings/to_department_abbreviations.json'),
+	departmentAbbrToName: require('mappings/to_department_names.json'),
+	gereqTitleToAbbr: require('mappings/to_gereq_abbreviations.json'),
+}
