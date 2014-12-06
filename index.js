94edb40e2a16ce44fc72ba623732fe47cbc76702
Hawken Rives <hawkrives@gmail.com>
2014-12-05 20:54:59 -0600

    Automatically maintain lists of data from the courses

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ module.exports = {
 	departmentNameToAbbr: require('mappings/to_department_abbreviations.json'),
 	departmentAbbrToName: require('mappings/to_department_names.json'),
 	gereqTitleToAbbr: require('mappings/to_gereq_abbreviations.json'),
+	valid: {
+		departments: require('mappings/valid_departments.json'),
+		gereqs: require('mappings/valid_gereqs.json'),
+		locations: require('mappings/valid_locations.json'),
+		professors: require('mappings/valid_professors.json'),
+		times: require('mappings/valid_times.json'),
+		types: require('mappings/valid_types.json'),
+	}
 }
